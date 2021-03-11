1d6ebf4e6d34a76d59af2ce7a064f4e3a924f109
Kimberley Challis <kimchallis@gmail.com>
2021-03-11 17:16:43 +0000

    Format date and time returned by API

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,19 +1,45 @@
 import React from "react";
 
 export default function FormattedDate(props) {
-  // const days = [
-  //   "Sunday",
-  //   "Monday",
-  //   "Tuesday",
-  //   "Wednesday",
-  //   "Thursday",
-  //   "Friday",
-  //   "Saturday",
-  // ];
-
-  // const day = days[props.rawDateTime.getDay()];
-
   const rawDateTime = new Date(props.rawDateTime);
 
-  return rawDateTime;
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const day = days[rawDateTime.getDay()];
+  const date = rawDateTime.getDate();
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const month = months[rawDateTime.getMonth()];
+
+  const year = rawDateTime.getFullYear();
+
+  const hour = rawDateTime.getHours();
+  let minutes = rawDateTime.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  const formattedDateTime = `${day}, ${date} ${month} ${year} ${hour}:${minutes}`;
+
+  return formattedDateTime;
 }
