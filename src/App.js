ead07598373e5b63b35f821ee0cd77eec41e83ba
Kimberley Challis <kimchallis@gmail.com>
2021-03-26 15:00:41 +0000

    Removed useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Search from "./Search";
 import Forecast from "./Forecast";
 
@@ -90,35 +90,37 @@ export default function App() {
 
   return (
     <div className="container">
-      <div className="App">
-        <div className="app shadow-lg">
-          <Search defaultCity="London" unit="metric" />
+      <div className="center">
+        <div className="App">
+          <div className="app shadow-lg">
+            <Search defaultCity="London" unit="metric" />
 
-          <Forecast />
+            <Forecast />
 
-          <div className="SwitchUnits">
-            <div className="row">
-              <div className="col">
-                <h4>
-                  <i className="fas fa-sync-alt"></i> switch to fahrenheit
-                </h4>
+            <div className="SwitchUnits">
+              <div className="row">
+                <div className="col">
+                  <h4>
+                    <i className="fas fa-sync-alt"></i> switch to fahrenheit
+                  </h4>
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="source">
-          <a
-            href="https://github.com/kimberleychallis/shecodes-plus-react"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Open Source
-          </a>
-          &nbsp;code by 👩🏼‍💻 Kimberley Challis for&nbsp;
-          <a href="https://shecodes.io" target="_blank" rel="noreferrer">
-            SheCodes.io
-          </a>
+          <div className="source">
+            <a
+              href="https://github.com/kimberleychallis/shecodes-plus-react"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open Source
+            </a>
+            &nbsp;code by 👩🏼‍💻 Kimberley Challis for&nbsp;
+            <a href="https://shecodes.io" target="_blank" rel="noreferrer">
+              SheCodes.io
+            </a>
+          </div>
         </div>
       </div>
     </div>
