5981c19ccb1b69a3613be2d4999a459590ec2c11
Kimberley Challis <kimchallis@gmail.com>
2021-04-01 15:14:02 +0100

    DayForecast basics done (API not working yet)

--- src/DayForecast.js
+++ src/DayForecast.js
@@ -1,20 +1,30 @@
-import React from "react";
-// import "./Forecast.css";
+import React, { useEffect, useState } from "react";
+import Icon from "./Icon";
 
 export default function DayForecast(props) {
-  // return <div>Min temp: {props.daily[2].temp.min}°</div>;
+  const [minTemperature, setMinTemperature] = useState(0);
+  const [maxTemperature, setMaxTemperature] = useState(0);
+
+  useEffect(() => {
+    // setMinTemperature(Math.round(props.data.daily[props.day].temp.min));
+    // setMaxTemperature(Math.round(props.data.daily[props.day].temp.max));
+    console.log("Effect has run");
+  }, []);
+
   return (
     <div className="Forecast">
       <div className="row justify-content-center">
         <div className="col-6">
           <h3>Name of day</h3>
         </div>
-        <div className="col-2 icon">Icon</div>
-        <div className="col-1 d-flex justify-content-center min-max">
-          <span class="max">15</span>
+        <div className="col-2 icon">
+          {/* <Icon weatherID={800} hour={16} /> */}Icon
         </div>
         <div className="col-1 d-flex justify-content-center min-max">
-          <span className="min">9</span>
+          <span className="max">{maxTemperature}</span>
+        </div>
+        <div className="col-1 d-flex justify-content-center min-max">
+          <span className="min">{minTemperature}</span>
         </div>
       </div>
     </div>
--- src/Forecast.js
+++ src/Forecast.js
@@ -5,36 +5,32 @@ import DayForecast from "./DayForecast";
 import "./Forecast.css";
 
 export default function Forecast(props) {
-  // const [loaded, setLoaded] = useState(false);
   const [forecast, setForecast] = useState({});
-  const apiKey = "3e11ec91583e0c90e17fc5eef84e88aa";
-  const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${props.latitude}&lon=${props.longitude}&exclude=current,minutely,hourly,alerts&appid=${apiKey}&units=metric`;
 
-  function handleResponse(response) {
-    // console.log(response.data);
-    setForecast(response.data);
-    // setLoaded(true);
-  }
+  const apiKey = "3e11ec91583e0c90e17fc5eef84e88aa";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${props.latitude}&lon=${props.longitude}&exclude=minutely,hourly,alerts&appid=${apiKey}&units=metric`;
 
   useEffect(() => {
-    // console.log(apiUrl);
+    console.log("the API has been called");
     axios.get(apiUrl).then(handleResponse);
-  });
 
-  // useEffect(() => {
-  //   console.log("forecast has updated");
-  //   return <DayForecast data={forecast} />;
-  // }, [forecast]);
+    function handleResponse(response) {
+      // console.log(response.data);
+      setForecast(response.data);
+    }
+  }, [apiUrl]);
 
   return (
     <div>
-      <DayForecast data={forecast} index={0} />
-      <DayForecast data={forecast} index={1} />
-      <DayForecast data={forecast} index={2} />
-      <DayForecast data={forecast} index={3} />
-      <DayForecast data={forecast} index={4} />
+      <DayForecast data={forecast} day={0} />
+      {/* <DayForecast data={forecast} day={1} />
+      <DayForecast data={forecast} day={2} />
+      <DayForecast data={forecast} day={3} />
+      <DayForecast data={forecast} day={4} /> */}
     </div>
   );
+
+  // ⬇️ FUNCTIONALITY WITHOUT HOOKS
 
   // if (!loaded) {
   //   const apiKey = "3e11ec91583e0c90e17fc5eef84e88aa";
